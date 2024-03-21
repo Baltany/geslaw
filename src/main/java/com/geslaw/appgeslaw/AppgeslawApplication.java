5d07be34fe32baa823c6d0bbc992f2f89f40713a
Balbino <93772318+Baltany@users.noreply.github.com>
2024-03-21 13:40:02 +0100

    Second commit

--- src/main/java/com/geslaw/appgeslaw/AppgeslawApplication.java
+++ src/main/java/com/geslaw/appgeslaw/AppgeslawApplication.java
@@ -2,16 +2,15 @@ package com.geslaw.appgeslaw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.flywaydb.core.Flyway;
 
 @SpringBootApplication
 public class AppgeslawApplication {
 
 	public static void main(String[] args) {
 
-		Flyway flwy = Flyway.configure().dataSource("jdbc:mysql://localhost:3306/indicadores","root","").load();
+		// Flyway flwy = Flyway.configure().dataSource("jdbc:mysql://localhost:8080/indicadores","root","root").load();
 
-		flwy.migrate();
+		// flwy.migrate();
 
 
 		SpringApplication.run(AppgeslawApplication.class, args);
