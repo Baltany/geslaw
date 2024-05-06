9e4c0a929a60ff61c2c2b68483f2b628775d1bb8
Balbino <93772318+Baltany@users.noreply.github.com>
2024-05-06 13:34:13 +0200

    Intentando hacer el envio de email

--- src/main/java/com/geslaw/appgeslaw/controller/EmailController.java
+++ src/main/java/com/geslaw/appgeslaw/controller/EmailController.java
@@ -6,34 +6,59 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 
 
 
-@Configuration
-@RequestMapping("/enviar-correo")
+@RestController
+@RequestMapping("/correo")
 public class EmailController {
     
+    @Autowired
+    private JavaMailSender mailSender;
 
-    public JavaMailSender getJavaMailSender(){
-        JavaMailSender mailSender = new JavaMailSenderImpl();
+    @PostMapping("/enviar")
+    public String enviarCorreo(@RequestBody String[] destinatarios,@RequestBody String asunto,@RequestBody String mensaje) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(destinatarios);
+            mailMessage.setSubject(asunto);
+            mailMessage.setText(mensaje);
 
-        Properties props = new Properties();
-        props.put("mail.transport.protocol", "smtp");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+            mailSender.send(mailMessage);
 
-        return mailSender;
-
+            return "Correo enviado correctamente";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error al enviar el correo";
+        }
     }
+
+    // public JavaMailSender getJavaMailSender(){
+    //     JavaMailSender mailSender = new JavaMailSenderImpl();
+
+    //     Properties props = new Properties();
+    //     props.put("mail.transport.protocol", "smtp");
+    //     props.put("mail.smtp.auth", "true");
+    //     props.put("mail.smtp.starttls.enable", "true");
+    //     props.put("mail.debug", "true");
+
+    //     return mailSender;
+
+    // }
 
 
 }
